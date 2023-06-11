4eb280adf48a5c043ceb0f45c713355b7e87b599
Ibramsou <kasobanboui@gmail.com>
2023-06-11 17:45:33 +0200

    Fix 1.19.4 system chat support

--- src/main/java/ua/realalpha/itsmyconfig/PacketChatListener.java
+++ src/main/java/ua/realalpha/itsmyconfig/PacketChatListener.java
@@ -1,20 +1,17 @@
 package ua.realalpha.itsmyconfig;
 
 import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
-import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.events.*;
 import com.comphenix.protocol.utility.MinecraftVersion;
 import com.comphenix.protocol.wrappers.WrappedChatComponent;
 import me.clip.placeholderapi.PlaceholderAPI;
 import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.chat.ComponentSerializer;
-import org.apache.commons.lang.StringUtils;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import ua.realalpha.itsmyconfig.model.ModelType;
 import ua.realalpha.itsmyconfig.xml.Tag;
@@ -26,36 +23,53 @@ import java.util.stream.Collectors;
 public class PacketChatListener extends PacketAdapter {
 
     private final ModelRepository modelRepository;
-    private ItsMyConfig itsMyConfig;
+    private final ItsMyConfig itsMyConfig;
     private final Pattern filtreColorSection = Pattern.compile("[§&][a-zA-Z0-9]");
 
     public PacketChatListener(ItsMyConfig itsMyConfig, ModelRepository modelRepository) {
-        super(itsMyConfig, ListenerPriority.NORMAL, (!MinecraftVersion.CAVES_CLIFFS_2.isAtLeast(MinecraftVersion.getCurrentVersion()) ? PacketType.Play.Server.SYSTEM_CHAT : PacketType.Play.Server.CHAT));
+        super(itsMyConfig, ListenerPriority.NORMAL, (!MinecraftVersion.CAVES_CLIFFS_2.isAtLeast(MinecraftVersion.getCurrentVersion()) ? PacketType.Play.Server.CHAT : PacketType.Play.Server.SYSTEM_CHAT));
         this.itsMyConfig = itsMyConfig;
         this.modelRepository = modelRepository;
     }
+
 
     @Override
     public void onPacketSending(PacketEvent event) {
         PacketContainer packetContainer = event.getPacket();
         Player player = event.getPlayer();
-        BaseComponent[] baseComponents;
-        if (!MinecraftVersion.CAVES_CLIFFS_2.isAtLeast(MinecraftVersion.getCurrentVersion())) {
-            String s = packetContainer.getStrings().readSafely(0);
-            if (s == null) return;
-            baseComponents = ComponentSerializer.parse(s);
-        }else {
-            WrappedChatComponent wrappedChatComponent = packetContainer.getChatComponents().readSafely(0);
-            if (wrappedChatComponent == null || (packetContainer.getChatComponentArrays().readSafely(0) != null && packetContainer.getChatComponentArrays().readSafely(0).length != 0)) return;
+        String message = null;
+        if (!MinecraftVersion.FEATURE_PREVIEW_2.isAtLeast(MinecraftVersion.getCurrentVersion())) {
 
-            baseComponents = ComponentSerializer.parse(wrappedChatComponent.getJson());
+            BaseComponent[] baseComponents;
+            if (!MinecraftVersion.CAVES_CLIFFS_2.isAtLeast(MinecraftVersion.getCurrentVersion())) {
+                String s = packetContainer.getStrings().readSafely(0);
+                if (s == null) return;
+                baseComponents = ComponentSerializer.parse(s);
+            }else {
+                WrappedChatComponent wrappedChatComponent = packetContainer.getChatComponents().readSafely(0);
+                if (wrappedChatComponent == null || (packetContainer.getChatComponentArrays().readSafely(0) != null && packetContainer.getChatComponentArrays().readSafely(0).length != 0)) return;
+
+                baseComponents = ComponentSerializer.parse(wrappedChatComponent.getJson());
+            }
+
+
+            StringBuilder stringBuilder = new StringBuilder();
+            for (BaseComponent baseComponent : baseComponents) {
+                stringBuilder.append(baseComponent.toLegacyText());
+            }
+            message = stringBuilder.toString();
+        } else {
+            InternalStructure modifier = packetContainer.getStructures().readSafely(0);
+            if (modifier != null && modifier.getHandle() instanceof TextComponent) {
+                TextComponent component = (TextComponent) modifier.getHandle();
+                message = LegacyComponentSerializer.legacySection().serialize(component);
+            }
+
+
         }
 
-        StringBuilder stringBuilder = new StringBuilder();
-        for (BaseComponent baseComponent : baseComponents) {
-            stringBuilder.append(baseComponent.toLegacyText());
-        }
-        String message = stringBuilder.toString();
+        if (message == null) return;
+
         message = PlaceholderAPI.setPlaceholders(player, message);
         message = PlaceholderAPI.setBracketPlaceholders(player, message);
         String plainTextWithOutColor = message.replaceAll(filtreColorSection.pattern(), "");
