06140d068728e6bc2a80a38f342ddb7d6c72778d
iiAhmedYT <61851106+iiAhmedYT@users.noreply.github.com>
2024-06-25 22:53:06 +0300

    latest try

--- src/main/java/to/itsme/itsmyconfig/placeholder/type/ColoredTextPlaceholder.java
+++ src/main/java/to/itsme/itsmyconfig/placeholder/type/ColoredTextPlaceholder.java
@@ -1,17 +1,16 @@
 package to.itsme.itsmyconfig.placeholder.type;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
-import to.itsme.itsmyconfig.component.AbstractComponent;
 import to.itsme.itsmyconfig.placeholder.Placeholder;
 import to.itsme.itsmyconfig.placeholder.PlaceholderType;
-import to.itsme.itsmyconfig.util.Strings;
 import to.itsme.itsmyconfig.util.Utilities;
 
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.regex.Matcher;
 
 public class ColoredTextPlaceholder extends Placeholder {
 
@@ -37,42 +36,15 @@ public class ColoredTextPlaceholder extends Placeholder {
         super(PlaceholderType.COLORED_TEXT);
         this.miniText = value;
 
+        final Component component = Utilities.MM.deserialize(value);
         // legacy text creation
-        String copy = AbstractComponent.parse(Utilities.MM.deserialize(value)).toMiniMessage();
+        this.legacyText = LegacyComponentSerializer.legacyAmpersand().serialize(
+                component
+        );
 
-        Matcher matcher = Strings.TAG_PATTERN.matcher(copy);
-        while (matcher.find()) {
-            final String found = matcher.group();
-            final String replacement = DECORATIONS_PROPERTIES.get(found.toLowerCase());
-            if (replacement != null) {
-                copy = copy.replace(found, replacement);
-                matcher = Strings.TAG_PATTERN.matcher(copy);
-            }
-        }
-
-        matcher = Strings.TAGGED_HEX_PATTERN.matcher(copy);
-        while (matcher.find()) {
-            final String found = matcher.group();
-            copy = copy.replace(found, "&" + found.substring(found.contains("/") ? 2 : 1, found.length() - 1));
-            matcher = Strings.TAGGED_HEX_PATTERN.matcher(copy);
-        }
-
-        this.legacyText = copy;
-
-        Matcher hexMatcher = Strings.HEX_PATTERN.matcher(copy);
-        while (matcher.find()) {
-            final String found = hexMatcher.group();
-            final String hexColor = found.substring(1);
-            final StringBuilder minecraftFormat = new StringBuilder("§x");
-            for (int i = 0; i < hexColor.length(); i++) {
-                minecraftFormat.append("§").append(hexColor.charAt(i));
-            }
-
-            copy = copy.replace(found, minecraftFormat);
-            hexMatcher = Strings.HEX_PATTERN.matcher(copy);
-        }
-
-        this.consoleText = copy;
+        this.consoleText = LegacyComponentSerializer.legacySection().serialize(
+                component
+        );
     }
 
     @Override
