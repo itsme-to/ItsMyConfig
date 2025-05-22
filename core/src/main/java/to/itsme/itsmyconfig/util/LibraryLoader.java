165572e07dc987164c37e35e8299795ab298e306
Ahmed Waleed <61851106+iiAhmedYT@users.noreply.github.com>
2025-05-22 15:47:00 +0300

    Dont force load minimessage

--- core/src/main/java/to/itsme/itsmyconfig/util/LibraryLoader.java
+++ core/src/main/java/to/itsme/itsmyconfig/util/LibraryLoader.java
@@ -20,7 +20,7 @@ public enum LibraryLoader {
             "net.kyori",
             "adventure-text-minimessage",
             BuildParameters.ADVENTURE_VERSION,
-            () -> true /*!Reflections.findClass("net.kyori.adventure.text.Component")*/
+            () -> !Reflections.findClass("net.kyori.adventure.text.Component")
     ),
     // ========================================================= //
     // Adventure Bukkit Platfrom //
