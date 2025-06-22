9324ff039136b1fa4f5034e71b2c32155bfd316f
Ahmed Waleed <61851106+iiAhmedYT@users.noreply.github.com>
2025-06-23 01:48:29 +0300

    Update PEventsProcessor.java

--- core/src/main/java/to/itsme/itsmyconfig/processor/packetevents/PEventsProcessor.java
+++ core/src/main/java/to/itsme/itsmyconfig/processor/packetevents/PEventsProcessor.java
@@ -7,11 +7,24 @@ import net.kyori.adventure.text.Component;
 import org.jetbrains.annotations.NotNull;
 import to.itsme.itsmyconfig.processor.PacketContent;
 import to.itsme.itsmyconfig.processor.PacketProcessor;
+import to.itsme.itsmyconfig.util.AdventureUtil;
 import to.itsme.itsmyconfig.util.IMCSerializer;
+
+import java.lang.reflect.Method;
 
 public class PEventsProcessor {
 
     public static final PacketProcessor<WrapperPlayServerChatMessage> CHAT_MESSAGE = new PacketProcessor<>() {
+        private final Method setChatContent;
+
+        {
+            try {
+                Class<?> chatMessageClass = Class.forName("com.github.retrooper.packetevents.wrapper.play.server.ChatMessage");
+                setChatContent = chatMessageClass.getMethod("setChatContent", AdventureUtil.getComponentClass());
+            } catch (Throwable t) {
+                throw new RuntimeException("Failed to resolve setChatContent method", t);
+            }
+        }
 
         @Override
         public String name() {
@@ -20,16 +33,33 @@ public class PEventsProcessor {
 
         @Override
         public void edit(WrapperPlayServerChatMessage wrappedPacket, Component component) {
-            wrappedPacket.getMessage().setChatContent(component);
+            Object chatContent = AdventureUtil.fromComponent(component);
+            Object chatMessage = wrappedPacket.getMessage();
+            try {
+                setChatContent.invoke(chatMessage, chatContent);
+            } catch (Throwable t) {
+                throw new RuntimeException("Failed to invoke setChatContent", t);
+            }
         }
 
         @Override
         public @NotNull PacketContent<WrapperPlayServerChatMessage> unpack(WrapperPlayServerChatMessage wrappedPacket) {
-            return new PacketContent<>(wrappedPacket, this, IMCSerializer.toMiniMessage(wrappedPacket.getMessage().getChatContent()));
+            Object chatContent = wrappedPacket.getMessage().getChatContent();
+            Component internal = AdventureUtil.toComponent(chatContent);
+            return new PacketContent<>(wrappedPacket, this, IMCSerializer.toMiniMessage(internal));
         }
     };
 
     public static final PacketProcessor<WrapperPlayServerSystemChatMessage> SYSTEM_CHAT_MESSAGE = new PacketProcessor<>() {
+        private final Method setMessage;
+
+        {
+            try {
+                setMessage = WrapperPlayServerSystemChatMessage.class.getMethod("setMessage", AdventureUtil.getComponentClass());
+            } catch (Throwable t) {
+                throw new RuntimeException("Failed to resolve setMessage method", t);
+            }
+        }
 
         @Override
         public String name() {
@@ -38,16 +68,32 @@ public class PEventsProcessor {
 
         @Override
         public void edit(WrapperPlayServerSystemChatMessage wrappedPacket, Component component) {
-            wrappedPacket.setMessage(component);
+            Object externalComponent = AdventureUtil.fromComponent(component);
+            try {
+                setMessage.invoke(wrappedPacket, externalComponent);
+            } catch (Throwable t) {
+                throw new RuntimeException("Failed to invoke setMessage", t);
+            }
         }
 
         @Override
         public @NotNull PacketContent<WrapperPlayServerSystemChatMessage> unpack(WrapperPlayServerSystemChatMessage wrappedPacket) {
-            return new PacketContent<>(wrappedPacket, this, IMCSerializer.toMiniMessage(wrappedPacket.getMessage()));
+            Object externalComponent = wrappedPacket.getMessage();
+            Component internal = AdventureUtil.toComponent(externalComponent);
+            return new PacketContent<>(wrappedPacket, this, IMCSerializer.toMiniMessage(internal));
         }
     };
 
     public static final PacketProcessor<WrapperPlayServerDisconnect> DISCONNECT = new PacketProcessor<>() {
+        private final Method setReason;
+
+        {
+            try {
+                setReason = WrapperPlayServerDisconnect.class.getMethod("setReason", AdventureUtil.getComponentClass());
+            } catch (Throwable t) {
+                throw new RuntimeException("Failed to resolve setReason method", t);
+            }
+        }
 
         @Override
         public String name() {
@@ -56,13 +102,19 @@ public class PEventsProcessor {
 
         @Override
         public void edit(WrapperPlayServerDisconnect wrappedPacket, Component component) {
-            wrappedPacket.setReason(component);
+            Object externalComponent = AdventureUtil.fromComponent(component);
+            try {
+                setReason.invoke(wrappedPacket, externalComponent);
+            } catch (Throwable t) {
+                throw new RuntimeException("Failed to invoke setReason", t);
+            }
         }
 
         @Override
         public @NotNull PacketContent<WrapperPlayServerDisconnect> unpack(WrapperPlayServerDisconnect wrappedPacket) {
-            return new PacketContent<>(wrappedPacket, this, IMCSerializer.toMiniMessage(wrappedPacket.getReason()));
+            Object externalComponent = wrappedPacket.getReason();
+            Component internal = AdventureUtil.toComponent(externalComponent);
+            return new PacketContent<>(wrappedPacket, this, IMCSerializer.toMiniMessage(internal));
         }
     };
-
 }
